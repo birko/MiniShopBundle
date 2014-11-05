04c870a2753e0cfde9e44d1705d34c6a8ddb44e1
František Bereň <birko@live.com>
2014-11-05 12:43:34 +0100

    setting to show disabled categories

--- Site/CategoryBundle/DependencyInjection/Configuration.php
+++ Site/CategoryBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,10 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('site_category');
+        $rootNode = $treeBuilder->root('site_category')
+                    ->children()
+                        ->booleanNode('show_disabled')->defaultTrue()->end()
+                    ->end;
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
